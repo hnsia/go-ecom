116b329c53a4aec693795987773453575d74614f
hnsia <44498178+hnsia@users.noreply.github.com>
2024-06-08 23:56:04 +0800

    feat: setting up env and db

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/hnsia/go-ecom/cmd/api"
+	"github.com/hnsia/go-ecom/config"
+	"github.com/hnsia/go-ecom/db"
 )
 
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Addr: config.Envs.DBAddress,
+		DBName: config.Envs.DBName,
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
