a22d26983255f2a29752df54bc9a57dbee6905b4
hnsia <44498178+hnsia@users.noreply.github.com>
2024-06-21 21:44:19 +0800

    feat: implement create product endpoint

--- service/product/routes.go
+++ service/product/routes.go
@@ -1,8 +1,10 @@
 package product
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"github.com/hnsia/go-ecom/types"
 	"github.com/hnsia/go-ecom/utils"
@@ -17,10 +19,11 @@ func NewHandler(store types.ProductStore) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
-	router.HandleFunc("/products", h.handleCreateProduct).Methods(http.MethodGet)
+	router.HandleFunc("/products", h.handleGetProduct).Methods(http.MethodGet)
+	router.HandleFunc("/products", h.handleCreateProduct).Methods(http.MethodPost)
 }
 
-func (h *Handler) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) handleGetProduct(w http.ResponseWriter, r *http.Request) {
 	ps, err := h.store.GetProducts()
 	if err != nil {
 		utils.WriteError(w, http.StatusInternalServerError, err)
@@ -28,4 +31,35 @@ func (h *Handler) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
 	}
 
 	utils.WriteJSON(w, http.StatusOK, ps)
+}
+
+func (h *Handler) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.CreateProductPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
+
+	// create new product
+	err := h.store.CreateProduct(types.Product{
+		Name: payload.Name,
+		Description: payload.Description,
+		Image: payload.Image,
+		Price: payload.Price,
+		Quantity: payload.Quantity,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
