a0101ad088e9f38e8c25e4d27990b66259702088
hnsia <44498178+hnsia@users.noreply.github.com>
2024-06-22 13:58:38 +0800

    feat: scaffold order service

--- types/types.go
+++ types/types.go
@@ -13,6 +13,29 @@ type ProductStore interface {
 	CreateProduct(Product) error
 }
 
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
+}
+
+type Order struct {
+	ID			int			`json:"id"`
+	UserID		int			`json:"userID"`
+	Total		float64		`json:"total"`
+	Status		string		`json:"status"`
+	Address		string		`json:"address"`
+	createdAt	time.Time	`json:"createdAt"`
+}
+
+type OrderItem struct {
+	ID			int			`json:"id"`
+	OrderID		int			`json:"orderID"`
+	ProductID	int			`json:"productID"`
+	Quantity	int			`json:"quantity"`
+	Price		float64		`json:"price"`
+	createdAt	time.Time	`json:"createdAt"`
+}
+
 type Product struct {
 	ID				int			`json:"id"`
 	Name			string		`json:"name"`
